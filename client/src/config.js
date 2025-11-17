76d7f1e47a367e1782ac514916421a7b4bf414bd
stephen-otieno <stephenotieno8781@gmail.com>
2025-11-17 16:29:04 +0300

    api config

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,4 @@
-export const API_URL = import.meta.env.VITE_BACKEND_URL;
+// export const API_URL = import.meta.env.VITE_API_URL;
+
+export const API_URL = "https://blogapp-backend-31p9.onrender.com/";
+
